4d7a13c5013f64954ccbebfbf93691e9fe942dea
deartc <4deartc@gmail.com>
2021-06-08 19:26:51 -0400

    delete coversion comment

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ b = datetime.datetime(2021, 5, 22, 8, 21, 10)
 c = a - b
 print('Difference between Fitness Start Date and End Date: ', c)
 
+100
 
 # This is code converting Kilograms into pounds and calories
 
@@ -217,66 +218,5 @@ while player == False:
     computer = t[randint(0, 2)]
 
 
-# This is a a conversion of kilograms to pounds
 
-# Then we will convert pounds to calories and post only the calories
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    import tkinter as tk
-
-    def main():
-        window = tk.Tk()
-        window.title("Kilograms to Pounds to Calories Converter")
-        window.geometry("375x200")
-
-        # create a label with text Enter Kilograms
-        label1 = tk.Label(window, text="Enter Kilograms:")
-
-        # create a label with text Pounds:
-        label2 = tk.Label(window, text="Pounds to Calories:")
-
-        # place label1 in window at position x,y
-        label1.place(x=50, y=30)
-
-        # create an Entry widget (text box)
-
-        textbox1 = tk.Entry(window, width=12)
-
-        # place textbox1 in window at position x,y
-        textbox1.place(x=200, y=35)
-
-        # place label2 in window at position x,y
-        label2.place(x=50, y=100)
-
-        # create a label3 with empty text:
-        label3 = tk.Label(window, text=" ")
-
-        # place label3 in window at position x,y
-        label3.place(x=180, y=100)
-
-        def btn1_click():
-            pounds = round(float(textbox1.get()) * 2.20462 * 3500, 5)
-            label3.configure(text=str(pounds) + '  Calories')
-
-        # create a button with text Button 1
-        btn1 = tk.Button(window, text="Click Me To Convert", command=btn1_click)
-        # place this button in window at position x,y
-        btn1.place(x=90, y=150)
-        window.mainloop()
-
-    main()
-
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('Dancers')
 
